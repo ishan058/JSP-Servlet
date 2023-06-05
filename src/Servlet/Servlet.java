3141d99b9d819abda81a8d7eed9ff33444102218
ishan058 <ishankhadkaik246@gmail.com>
2023-06-05 09:29:36 +0545

    finish

--- src/Servlet/Servlet.java
+++ src/Servlet/Servlet.java
@@ -1,14 +1,17 @@
 package Servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/hello")
 public class Servlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res){
-        req.getRequestDispatcher("index.jsp");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setAttribute("name", "noice");
+        req.getRequestDispatcher("index.jsp").forward(req, res);
     }
 
 }
--- webapp/index.jsp
+++ webapp/index.jsp
@@ -1,6 +1,6 @@
 <html>
     <head></head>
     <body>
-        <h1>hello</h1>
+        <h1>hello, This is ${name}</h1>
     </body>
 </html>
