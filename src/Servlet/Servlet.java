1b998571b5f9b6ebb3fbc0c688ace92d430235b3
ishan058 <ishankhadkaik246@gmail.com>
2023-06-05 08:49:35 +0545

    naya

--- src/Servlet/Servlet.java
+++ src/Servlet/Servlet.java
@@ -1,5 +1,14 @@
 package Servlet;
 
-public class Servlet {
-    
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello")
+public class Servlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res){
+        req.getRequestDispatcher("index.jsp");
+    }
+
 }
